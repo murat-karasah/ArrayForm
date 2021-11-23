5a5502d1f1fea9fa0c07ff9247ca3a6e28ff1954
Murat Karasah <mkarasah@gmail.com>
2021-11-23 10:30:26 +0300

    Add project files.

---
+++ ArrayForm/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace ArrayForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.txtPush = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pushName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(54, 10);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(54, 38);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(100, 20);
+            this.txtNote.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Not";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(54, 65);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 95);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(54, 251);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(100, 23);
+            this.btnPush.TabIndex = 10;
+            this.btnPush.Text = "Getir";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // txtPush
+            // 
+            this.txtPush.Enabled = false;
+            this.txtPush.Location = new System.Drawing.Point(54, 280);
+            this.txtPush.Name = "txtPush";
+            this.txtPush.Size = new System.Drawing.Size(100, 20);
+            this.txtPush.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Not";
+            // 
+            // pushName
+            // 
+            this.pushName.Location = new System.Drawing.Point(54, 225);
+            this.pushName.Name = "pushName";
+            this.pushName.Size = new System.Drawing.Size(100, 20);
+            this.pushName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(164, 315);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.txtPush);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pushName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.TextBox txtPush;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pushName;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ArrayForm/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayForm
+{
+    public partial class Form1 : Form
+    {
+      public  string[] students = new string[5];
+        public int[] note = new int[5];
+        public int index;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            students[index] = txtName.Text;
+            note[index] = Convert.ToInt32(txtNote.Text);
+            listBox1.Items.Add("Adı:" + students[index] + " not:" + note[index]);
+
+            index++;
+            if (index==5)
+            {
+                btnSave.Enabled = false;
+            }
+        }
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            string arama = pushName.Text;
+            int index1 = Array.IndexOf(students,arama);
+            int asd = note[index1]; 
+           txtPush.Text=  asd.ToString();
+            
+        }
+    }
+}
